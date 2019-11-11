0ca77f565b3f620693fbcc88e45fbce747500c32
marciaalegria <ytasegundo@gmail.com>
2019-11-11 10:21:09 -0300

    formulario

--- test/model-data.spec.js
+++ test/model-data.spec.js
@@ -3,3 +3,8 @@ import {suma} from '../src/js/model-data';
 test('sumar 1 + 2 es igual a 3', () => {
   expect(suma(1, 2)).toBe(3);
 });
+
+
+describe("validador de contraseñas", ()=>{
+  it("debe verificar que la contraseña sea mas de 6 caracteres")
+});
